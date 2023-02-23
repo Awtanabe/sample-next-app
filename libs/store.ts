f2cd29671cef3352c0d39c26708c7420bad71581
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-02-23 10:48:20 +0900

    Todoの追加

--- libs/store.ts
+++ libs/store.ts
@@ -9,19 +9,15 @@ type Todo = {
 
 export type todoStore = {
   todos: Todo[];
-  fetchTodos: () => Promise<Todo[]>;
   addTodo: (todo: Todo) => void;
 }
 
 export const useStore = create<todoStore>((set, get) => ({
   todos: [],
-  fetchTodos: async () => {
-    const response = await axios('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => res.data)
-    return response
-  },
   addTodo: (todo) => {
     const newTodo = {...todo}
-    set((state) => ({ todos: [...state.todos, newTodo] }))
+    set((state) => {
+      return { todos: [...state.todos, newTodo] }
+    })
   },
 }))
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,29 +9,52 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const store = useStore();
-  const { todos, fetchTodos, addTodo} = useStore((state) => state)
+  const { todos, addTodo} = useStore((state) => state)
+  const [ todoForm, setTodoForm ] = useState<todoStore['todos'][0] | null>()
+
+  const [ title, setTitle ] = useState<string>("");
+  const [ completed, setCompleted ] = useState<boolean>(false);
+
+  const changeTitleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value)
+  }
+
+  const changeCompletedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompleted(e.target.checked)
+  }
+
+  const submitTodo = (e: React.FormEvent<HTMLFormElement>, addTodo: todoStore['addTodo'], todos: todoStore['todos'], title: todoStore['todos'][0]['title'], completed: todoStore['todos'][0]['completed']) => {
+    e.preventDefault();
+    const id = todos.length += 1;
+    const newTodo = { id, title, completed }
+    addTodo(newTodo)
+    setTitle('')
+    setCompleted(false)
+  }
 
   const renderTodos = (todos: todoStore['todos']) => {
     if (todos.length == 0) {
       return "Todoを登録してください"
     } else {
-      todos?.map((todo) => {
-        return <li key={todo.id}>{todo.title}</li>
-      })
+      const newTodos = todos.filter(v => v)
+      return newTodos.map((todo) => (<li key={todo.id}>{todo.title}</li>))
     }
   }
   return (
     <>
     <div className={styles.main}>
       <div>TodoApp</div>
-      <form>
-        <input type="text" />
+      <form onSubmit={(e) => submitTodo(e,addTodo, todos, title, completed)}>
+        <input type="text"  value={title} onChange={changeTitleHandler}/>
         <br />
-        <input type="text" name="title"/>
+        <input type="checkbox" checked={completed} onChange={changeCompletedHandler}/>
         <br />
         <input type="submit"/>
       </form>
-      {renderTodos(todos)}
+
+      <ul>
+        {renderTodos(todos)}
+      </ul>
     </div>
     </>
   )
