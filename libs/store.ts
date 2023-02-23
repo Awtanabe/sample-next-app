e4846fc6e4f5f659a59b29a4e624f6ee46f4e787
akifumi_watanabe <akifumi_watanabe@akifumi-watanabenonotopasokon.local>
2023-02-23 09:17:42 +0900

    不要なコード削除

--- libs/store.ts
+++ libs/store.ts
@@ -1,21 +1,5 @@
 import create from "zustand";
-import axios, { AxiosResponse } from "axios";
-import { SetStateAction } from "react";
-
-type bearStore = {
-	bears: number;
-	increasePopulation: () => void;
-	removeAllBears: () => void;
-}
-
-
-
-
-// export const useBearStore = create<bearStore>((set) => ({
-//   bears: 0,
-//   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-//   removeAllBears: () => set({ bears: 0 }),
-// }))
+import axios from "axios";
 
 type Todo = {
   userId: number;
@@ -28,13 +12,6 @@ export type todoStore = {
   todos: Todo[];
   fetchTodos: () => Promise<Todo[]>;
   addTodo: (todo: Todo) => void;
-}
-// Promise<Response>
-
-// https://jsonplaceholder.typicode.com/todos
-const fetchTodos = async () => {
-  // axios('https://jsonplaceholder.typicode.com/todos')
-  // .then((res) => res.data)
 }
 
 export const useStore = create<todoStore>((set, get) => ({
